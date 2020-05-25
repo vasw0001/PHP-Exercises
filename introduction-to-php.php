68caeb2375b330fce4858dc67930ec0734074690
vasw0001 <65926478+vasw0001@users.noreply.github.com>
2020-05-25 19:36:51 -0400

    Assignment 1 - Online Class: Introduction to PHP

---
+++ introduction-to-php.php
@@ -0,0 +1,89 @@
+<?php
+/*
++---+
+| 1 | 
++---+
+Print the sentence: Welcome to PHP!
+*/
+echo "Welcome to PHP!";
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 2 | 
++---+
+Print the sentence: PHP stands for "Hypertext Preprocessor"!
+*/
+echo "\"Hypertext Preprocessor\"!";
+
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 3 | 
++---+
+Print the following sentence as a paragraph: PHP stands for "Hypertext Preprocessor"!
+*/
+echo "<p> \"PHP stands for Hypertext Processor\"! </p>";
+
+
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 4 | 
++---+
+Print the following acronyms - every acronym in a new line:
+
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/
+echo "HTML - Stands for Hypertext Markup Language";   
+echo "<br />";
+echo "CSS - Stands for Cascading Stylesheet";
+echo "<br />";
+echo "JS - Stands for JavaScript";
+echo "<br />";
+echo "PHP - Stands for Hypertext Preprocessor";
+echo "<br />";
+echo "SQL - Stands for Structural Query Language";
+echo "<br />";
+
+   
+   
+// task separator
+echo "<hr style=\"margin 1rem 0\">";
+
+/* 
++---+
+| 5 | 
++---+
+Print the following acronyms in unordered list - <ul> ... </ul>:
+
+HTML - Stands for Hypertext Markup Language
+CSS - Stands for Cascading Stylesheet
+JS - Stands for JavaScript
+PHP - Stands for Hypertext Preprocessor
+SQL - Stands for Structural Query Language
+*/
+echo "<ul>
+<li> HTML - Stands for Hypertext Markup Language </li>
+<li> CSS - Stands for Cascading Stylesheet </li>
+<li> JS - Stands for JavaScript </li>
+<li> PHP - Stands for Hypertext Preprocessor </li>
+<li> SQL - Stands for Structural Query Language </li>
+</ul>" 
+    
+?>
+
+
